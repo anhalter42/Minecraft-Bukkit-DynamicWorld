75d138f7c63163faf548e2c375a8f80b3e4c31d1
Andre Schloemp <andre.schloemp@me.com>
2012-05-16 19:28:21 +0200

    initial commit

---
+++ src/com/mahn42/anhalter42/dynamicworld/DynamicWorld.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mahn42.anhalter42.dynamicworld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author andre
+ */
+public class DynamicWorld extends JavaPlugin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    @Override
+    public void onEnable() {
+        
+    }
+}
